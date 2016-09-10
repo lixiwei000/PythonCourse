08f003460718e1c7e8951d2526aa4f0c1698e6b7
lxw <lxw>
2016-09-11 01:08:30 +0800

    daily

--- day2/File.py
+++ day2/File.py
@@ -1,6 +1,11 @@
 
 
 
-f = open("/etc/passwd")
-for line in f.readlines():
-    print(line.strip())
+f = open("E:\IdeaProjects\PythonCourse\README.md","r+")
+# for line in f.readlines():
+#     print(line.strip())
+
+# print(f.truncate(10))
+f.seek(16)
+print(f.tell())
+# print(f.readline())
