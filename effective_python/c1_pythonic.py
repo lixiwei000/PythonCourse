7d5181b3bf6a23a208080ff88f7bb44a84e968ef
lixiwei <13522781970@163.com>
2017-07-21 13:04:38 +0800

    chapter01 done.

--- effective_python/c1_pythonic.py
+++ effective_python/c1_pythonic.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # Tip1: Check Your Python Version Before Starting Work
+from flask import json
+
+
 def py_version():
     import sys
     print sys.version
@@ -72,5 +75,71 @@ def more_list_generator():
     print filtered
 
 
+# Tip9: Generator Expression
+def generator_expression():
+    l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    l_it = (x for x in l)
+    sqr_it = ((x, x ** 2) for x in l_it)
+    for x in sqr_it:
+        print x
+
+
+# Tip10: Enumerate
+def enum_instead_of_range():
+    from random import randint
+    # Range Is Useful while Handle Integers
+    random_bits = 0
+    for i in range(64):
+        if randint(0, 1):
+            random_bits |= 1 << i
+    print "============== Pretty Split Line =============="
+
+    flavor_list = ['coco', 'coke', 'KungPaoKiTen', 'Banana']
+    for flavor_index in range(len(flavor_list)):
+        print '%d : %s is delicious' % (flavor_index + 1, flavor_list[flavor_index])
+
+    print "============== Pretty Split Line =============="
+
+    for i, flavor in enumerate(flavor_list, 1):
+        print '%d : %s is delicious' % (i, flavor)
+
+
+# Tip11: Zip
+def iterator_multiple_collections_by_zip():
+    names = ['Niko', 'Tom', 'Helen']
+    ages = [21, 15, 25]
+    sizes = {'A': 35, 'B': 60, 'C': 22}
+    for name, age, size, in zip(names, ages, sizes):
+        print name, age, size
+
+
+# Tip12: For Else
+def for_else():
+    num_list = [x for x in range(100)]
+    for n in num_list:
+        print n
+        if n == 20:
+            break
+    else:
+        print 'for never break.'
+
+# Tip13: Try Except Else Finally
+def try_excep_else_finally():
+    f = open('/Users/lixiwei-mac/Documents/IdeaProjects/PythonStudy/effective_python/test.txt', 'r')
+    try:
+        data = f.read()
+        if len(data) < 100:
+            raise Exception("Shit ! This fucking file is too short")
+    except Exception as e:
+        raise ValueError(e)
+    else:
+        print 'There is no exception happend.'
+        print data
+        return data
+    finally:
+        print 'closing file handle'
+        f.close()
+
+
 if __name__ == "__main__":
-    more_list_generator()
+    try_excep_else_finally()
